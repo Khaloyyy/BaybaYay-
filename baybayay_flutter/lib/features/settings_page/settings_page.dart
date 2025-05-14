bdd90d0df9ec46b3841f5af294d64d1ae0958bff
iamKhaloyy <carlo.cabbo09@gmail.com>
2025-05-15 01:39:21 +0800

    Create settings_page.dart

---
+++ baybayay_flutter/lib/features/settings_page/settings_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+const SettingsPage({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          'Settings Page',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
