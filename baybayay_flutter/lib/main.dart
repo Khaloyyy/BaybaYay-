5a57e2b2c5ce4056260b720642d47adce8030b47
iamKhaloyy <carlo.cabbo09@gmail.com>
2025-05-17 04:28:39 +0800

    Update main.dart

--- baybayay_flutter/lib/main.dart
+++ baybayay_flutter/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:baybayay_flutter/features/home_page/homepage.dart';
+import 'package:baybayay_flutter/features/kasaysayan_page/kasaysayan_page.dart';
+import 'package:baybayay_flutter/features/settings_page/settings_page.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +13,37 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: HomePageScreen()
+     routerConfig: _router,
     );
   }
+
+
+
 }
+
+  final GoRouter _router = GoRouter(
+    initialLocation: 'home',
+    routes:<RouteBase> [
+       GoRoute(
+        name: 'settings',
+        path: '/settings',
+        builder: (context, state) => const SettingsPageScreen(),
+        // builder: (context, state) => const SplashScreen(),
+      ),
+
+      // ================= Splash Screen ================= //
+      GoRoute(
+        name: 'kasaysayan',
+        path: '/kasaysayan',
+        builder: (context, state) => const KasaysayanPageScreen(),
+      ),
+
+      GoRoute(
+        name: 'home',
+        path: '/home',
+        builder: (context, state) => const HomePageScreen(),
+      ),
+    ],
+  );
